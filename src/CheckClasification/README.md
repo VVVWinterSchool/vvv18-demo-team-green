dcdaa583da43face259ddd6ad3b7bbaf99956219
Juan David Munoz <yidiem@gmail.com>
2018-02-14 09:30:31 +0100

    beginnign from the torque assigment

---
+++ src/CheckClasification/README.md
@@ -0,0 +1,12 @@
+Computed Torque with iCub
+========================
+
+The module is always reading a boolean port 
+    - true means the robot can do nice stuffs.
+    - false: module does nothing
+    
+Output is another bool port
+    true if the human agrees with the classification the robot did
+    false if the human disagree with the classification the robot did
+    
+    
---
+++ src/CheckClasification/main.cpp
@@ -0,0 +1,23 @@
+#include "Module.h"
+#include <yarp/os/Network.h>
+
+int main(int argc, char **argv)
+{
+    using namespace yarp::os;
+    using namespace yarp::sig;
+
+    yarp::os::Network yarp;
+
+
+    if (!yarp::os::Network::checkNetwork(5.0)) {
+        std::cout << "Yarp network not found\n";
+        return false;
+    }
+
+    ResourceFinder rf = ResourceFinder::getResourceFinderSingleton();
+    rf.configure(argc, argv);
+    Module module;
+    module.setName("computed-torque");
+    
+    return module.runModule(rf);
+}
