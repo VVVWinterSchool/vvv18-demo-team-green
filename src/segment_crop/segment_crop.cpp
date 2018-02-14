929c5b3c4980e14b1efecd362191d14b3b342ea3
Ugo Vollhardt <ugo.vollhardt@gmail.com>
2018-02-14 15:02:06 +0100

    Basic implementation without onRead() method

--- src/segment_crop/segment_crop.cpp
+++ src/segment_crop/segment_crop.cpp
@@ -2,19 +2,28 @@
 
 bool Module::configure(yarp::os::ResourceFinder &rf)
 {
-    std::string moduleName = rf.check("name", yarp::os::Value("closest-blob"), "module name (string)").asString();
+    this->rf=&rf;
+    std::string moduleName = rf.check("name", yarp::os::Value("segment_crop"), "module name (string)").asString();
     setName(moduleName.c_str());
     inDispPort = new CustomProcessor(moduleName);
+    closing = false;
+
+    inDispPort->open();
+
     return true;
 }
 
 bool Module::close()
 {
+    inDispPort->interrupt();
+    inDispPort->close();
+    delete inDispPort;
     return true;
 }
 
 bool Module::quit()
 {
+    closing = true;
     return true;
 }
 
@@ -25,31 +34,35 @@ double Module::getPeriod()
 
 bool Module::updateModule()
 {
-    return true;
+    return !closing;
 }
 
 CustomProcessor::CustomProcessor(const std::string &moduleName)
 {
+    this->moduleName = moduleName;
+}
 
+bool CustomProcessor::open()
+{
+    this->useCallback();
+    this->BufferedPort<yarp::sig::ImageOf<yarp::sig::PixelMono> >::open( "/" + moduleName + "/disparity:i" );
+    inRGBPort.open("/" + moduleName + "/RGBimage:i");
+    outStuffPort.open("/"+ moduleName + "/crops:o");
+}
+
+void CustomProcessor::close()
+{
+    outStuffPort.close();
+    inRGBPort.close();
+    this->close();
+}
+
+void CustomProcessor::interrupt()
+{
+    this->interrupt();
 }
 
 void CustomProcessor::onRead(yarp::sig::ImageOf<yarp::sig::PixelMono> &dispImage )
 {
 
 }
-
-bool CustomProcessor::open()
-{
-
-}
-
-void CustomProcessor::close()
-{
-
-}
-
-void CustomProcessor::interrupt()
-{
-
-}
-
