b2ae0892caef1f7f4fa6e6ba5d9e9ee71b10d304
Ugo Vollhardt <ugo.vollhardt@gmail.com>
2018-02-14 20:00:51 +0100

    All implemented, need test on the robot

--- src/segment_crop/segment_crop.cpp
+++ src/segment_crop/segment_crop.cpp
@@ -85,6 +85,7 @@ void CustomProcessor::onRead(yarp::sig::ImageOf<yarp::sig::PixelMono> &dispImage
 {
     yarp::sig::ImageOf<yarp::sig::PixelRgb> &outDebugRGB = outDebugPortRGB.prepare();
     yarp::sig::ImageOf<yarp::sig::PixelRgb> *inRGB = inRGBPort.read();
+    yarp::os::Bottle &outBottle = outStuffPort.prepare();
 
     outDebugRGB.resize(dispImage.width(),dispImage.height());
     outDebugRGB.zero();
@@ -104,6 +105,8 @@ void CustomProcessor::onRead(yarp::sig::ImageOf<yarp::sig::PixelMono> &dispImage
 
     cv::Rect bounding_box_1;
     cv::Rect bounding_box_2;
+    yarp::sig::Vector centre_1;
+    yarp::sig::Vector centre_2;
     yarp::sig::Vector  object_1;
     yarp::sig::Vector  object_2;
 
@@ -111,6 +114,69 @@ void CustomProcessor::onRead(yarp::sig::ImageOf<yarp::sig::PixelMono> &dispImage
     {
         bounding_box_1 = cv::boundingRect(contours[0]);
         bounding_box_2 = cv::boundingRect(contours[1]);
+        centre_1[0] = (bounding_box_1.tl().x + bounding_box_1.br().x)/2;
+        centre_1[1] = (bounding_box_1.tl().y + bounding_box_1.br().y)/2;
+
+        centre_2[0] = (bounding_box_2.tl().x + bounding_box_2.br().x)/2;
+        centre_2[1] = (bounding_box_2.tl().y + bounding_box_2.br().y)/2;
+
+        yarp::os::Bottle cmd, response;
+
+        cmd.addString("Points");
+        cmd.addInt(centre_1[0]);
+        cmd.addInt(centre_1[1]);
+        cmd.addInt(centre_2[0]);
+        cmd.addInt(centre_2[1]);
+
+        yInfo() << "Querying SFM: " << cmd.toString().c_str();
+
+        queryClient.write(cmd,response);
+        yInfo() << "Got response : " << response.toString().c_str();
+
+        object_1.push_back(response.get(0).asDouble());
+        object_1.push_back(response.get(1).asDouble());
+        object_1.push_back(response.get(2).asDouble());
+        object_2.push_back(response.get(3).asDouble());
+        object_2.push_back(response.get(4).asDouble());
+        object_2.push_back(response.get(5).asDouble());
+
+        if (object_1[0] < -0.2 && object_2[0] < -0.2)
+        {
+            yarp::os::Bottle listObj1_Rect;
+            yarp::os::Bottle listObj2_Rect;
+            yarp::os::Bottle listObj1_pos;
+            yarp::os::Bottle listObj2_pos;
+
+            listObj1_Rect.addInt(bounding_box_1.tl().x);
+            listObj1_Rect.addInt(bounding_box_1.tl().y);
+            listObj1_Rect.addInt(bounding_box_1.br().x);
+            listObj1_Rect.addInt(bounding_box_1.br().x);
+
+            listObj2_Rect.addInt(bounding_box_2.tl().x);
+            listObj2_Rect.addInt(bounding_box_2.tl().y);
+            listObj2_Rect.addInt(bounding_box_2.br().x);
+            listObj2_Rect.addInt(bounding_box_2.br().x);
+
+            listObj1_pos.addDouble(object_1[0]);
+            listObj1_pos.addDouble(object_1[1]);
+            listObj1_pos.addDouble(object_1[2]);
+
+            listObj2_pos.addDouble(object_2[0]);
+            listObj2_pos.addDouble(object_2[1]);
+            listObj2_pos.addDouble(object_2[2]);
+
+            outBottle.clear();
+
+            outBottle.addList() = listObj1_Rect;
+            outBottle.addList() = listObj1_pos;
+            outBottle.addList() = listObj2_Rect;
+            outBottle.addList() = listObj2_pos;
+
+            yInfo() << outBottle.toString();
+
+            outStuffPort.write();
+
+        }
 
 
     }
