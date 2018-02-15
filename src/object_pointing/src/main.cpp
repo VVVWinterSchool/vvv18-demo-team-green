35d728311e448089cc88a0a87580dd0a45cf0ce0
Mihai ANDRIES <mih.andries@gmail.com>
2018-02-15 01:34:41 +0100

    Pointing works OK. TODO: add ports

--- src/object_pointing/src/main.cpp
+++ src/object_pointing/src/main.cpp
@@ -37,6 +37,8 @@ class CtrlModule: public RFModule
     BufferedPort<Bottle> inputPort;
     BufferedPort<Bottle> outputPort;
 
+    yarp::sig::Vector targetObjectPosition;
+
     PolyDriver         client;
     ICartesianControl *arm;
 
@@ -54,6 +56,8 @@ class CtrlModule: public RFModule
 
     bool threadCalled = false;
 
+    int tempTotalPointings = 0;
+
 public:
 
     void extendIndexFingerLength(double lengthToAdd)
@@ -63,9 +67,12 @@ public:
         // Vector in axis-angle representation
         yarp::sig::Vector orientation(4);
 
-        position[0] = lengthToAdd - 0.1; //0.1; // in meters
+        position[0] = lengthToAdd; //0.1; // in meters
         position[1] = -0.02;
         position[2] = 0.0;
+
+        // Add this length to a total cumulator
+        fingerAddedLength += lengthToAdd;
 
         // Orietnation with respect to the original frame
         orientation[0] = 0.0;
@@ -105,30 +112,44 @@ public:
         return x;
     }
 
-    // TODO FINISH!!!
-    yarp::sig::Vector getClosestReachablePointToObject(yarp::sig::Vector objectPosition,
+    // Get closest reachable pose to object (on the sphere shell which is the limit of reachability space)
+    yarp::sig::Vector getClosestReachablePoseToObject(yarp::sig::Vector objectPosition,
                                                        yarp::sig::Vector shoulderPosition,
                                                        double armLength)
     {
-        yarp::sig::Vector closestPoint(3);
+        yarp::sig::Vector closestPointPosition(3);
+        yarp::sig::Vector closestPointOrientation(3);
 
-        yarp::sig::Vector lineCoeffs(3);
-        yarp::sig::Vector lineOffsets(3);
-        lineCoeffs[0] = shoulderPosition[0] - objectPosition[0];
-        lineCoeffs[1] = shoulderPosition[1] - objectPosition[1];
-        lineCoeffs[2] = shoulderPosition[2] - objectPosition[2];
+        yarp::sig::Vector lineSlant(3);
+        yarp::sig::Vector lineOffsets(3); // this is shoulder position
+
+        lineSlant[0] = shoulderPosition[0] - objectPosition[0];
+        lineSlant[1] = shoulderPosition[1] - objectPosition[1];
+        lineSlant[2] = shoulderPosition[2] - objectPosition[2];
 
         // Compute the line offsets
-        linesOffsets[0] = ;
-        linesOffsets[1] = ;
-        linesOffsets[2] = ;
+        // it's the shoulder position
 
         // Compute the intersection of the line with the sphre centered on the shoulder
-        closestPoint[0] = ;
-        closestPoint[1] = ;
-        closestPoint[2] = ;
+        closestPointPosition[0] = armLength * lineSlant[0] + shoulderPosition[0];
+        closestPointPosition[1] = armLength * lineSlant[1] + shoulderPosition[1];
+        closestPointPosition[2] = armLength * lineSlant[2] + shoulderPosition[2];
 
-//        yarp::sig::Vector
+        closestPointOrientation[0] = lineSlant[0];
+        closestPointOrientation[1] = lineSlant[1];
+        closestPointOrientation[2] = lineSlant[2];
+
+        yarp::sig::Vector closestReachablePose(6);
+
+        closestReachablePose[0] = closestPointPosition[0];
+        closestReachablePose[0] = closestPointPosition[1];
+        closestReachablePose[0] = closestPointPosition[2];
+
+        closestReachablePose[0] = closestPointOrientation[0];
+        closestReachablePose[0] = closestPointOrientation[1];
+        closestReachablePose[0] = closestPointOrientation[2];
+
+        return closestPointPosition;
     }
 
     double getArmLength()
@@ -136,24 +157,6 @@ public:
         return 0.4;
     }
 
-
-
-    // Tells if a 3d position is reachable by the robot's arm
-    bool isInsideReachableSphere(yarp::sig::Vector objectPosition)
-    {
-        yarp::sig::Vector shoulderPosition = getShoulderPosition();
-        double distanceFromShoulderToObject = getInterPointDistance(shoulderPosition, objectPosition);
-        double armLength = getArmLength();
-
-        if (distanceFromShoulderToObject < armLength)
-        {
-            return true;
-        }
-        else // object out of reach
-        {
-            return false;
-        }
-    }
 
     double getInterPointDistance(yarp::sig::Vector p1, yarp::sig::Vector p2)
     {
@@ -163,35 +166,6 @@ public:
                     std::pow(p1[2] - p2[2], 2));
     }
 
-    virtual void run()
-    {
-        if (!threadCalled)
-        {
-            threadCalled = true;
-
-            t=Time::now();
-            // Generate a new target every (N) milliseconds
-            generateTarget();
-
-            bool objectIsInsideReachableSphere = isInsideReachableSphere(objectPosition);
-
-            if (objectIsInsideReachableSphere)
-            {
-                extendIndexFingerLength(lengthToAdd);
-                pointIndexFingerToTarget(objectPosition);
-                extendIndexFingerLength(-lengthToAdd);
-            }
-            else
-            {
-
-            }
-
-
-            // some verbosity
-            //printStatus();
-        }
-
-    }
 
 //    void printStatus()
 //    {
@@ -229,19 +203,19 @@ public:
 //    }
 
 
-    void generateTarget()
+    yarp::sig::Vector generateTarget()
     {
         // translational target part: a circular trajectory
         // in the yz plane centered in [-0.3,-0.1,0.1] with radius=0.1 m
         // and frequency 0.1 Hz
 
-        double MAX_DIST = - 1.5;
-        double MIN_DIST = - 0.2;
+        //double MAX_DIST = - 1.5;
+        //double MIN_DIST = - 0.2;
 
         //xd[0] = std::min((std::rand() * MAX_DIST), MIN_DIST);
-         xd[0]=-0.3;
-         xd[1]=-0.1+0.1*cos(2.0*M_PI*0.1*(t-t0));
-         xd[2]=+0.1+0.1*sin(2.0*M_PI*0.1*(t-t0));
+         xd[0]=-2.3 + (std::rand()% 2);
+         xd[1]=-1.3 + (std::rand()% 10)*0.17; //0.1+0.1*cos(2.0*M_PI*0.1*(t-t0));
+         xd[2]=+0.7 + (std::rand()%10)*0.12; //0.1; //0.1+0.1*sin(2.0*M_PI*0.1*(t-t0));
 
         yInfo()<<"New target: "<<xd[0]<<", "<<xd[1]<<", "<<xd[2];
 
@@ -249,7 +223,8 @@ public:
         // we want the middle finger to point forward (end-effector x-axis)
         // with the palm turned down (end-effector y-axis points leftward);
         // to achieve that it is enough to rotate the root frame of pi around z-axis
-        od[0]=0.0; od[1]=0.0; od[2]=1.0; od[3]=0; //M_PI;
+        //od[0]=0.0; od[1]=0.0; od[2]=1.0; od[3]=0; //M_PI;
+        return xd;
     }
 
     void limitTorsoPitch()
@@ -346,9 +321,6 @@ public:
         xd.resize(3);
         od.resize(4);
 
-        fingerAddedLength = 0.1; //xd[0];
-        //extendIndexFingerLength(fingerAddedLength);
-
         yInfo()<<"Thread started successfully";
         t=t0=t1=Time::now();
 
@@ -357,6 +329,7 @@ public:
 
     virtual bool close()
     {
+        yInfo()<<"Closing... ";
         // we require an immediate stop
         // before closing the client for safety reason
         arm->stopControl();
@@ -367,6 +340,7 @@ public:
 
         client.close();
 
+        yInfo()<<"done!";
         return true;
     }
 
@@ -377,7 +351,81 @@ public:
 
     virtual bool updateModule()
     {
-        yInfo()<<"Running happily...";
+        yInfo()<<"UpdateModule is called!";
+        if (!threadCalled)
+        {
+            yInfo()<<"Pointing operations start!";
+            threadCalled = true;
+
+            t=Time::now();
+            // Generate a new target every (N) milliseconds
+            targetObjectPosition = generateTarget(); // fills GD
+
+            yarp::sig::Vector shoulderPosition = getShoulderPosition();
+            double distanceFromShoulderToObject = getInterPointDistance(shoulderPosition, targetObjectPosition);
+            double armLength = getArmLength();
+
+            // Tells if a 3d position is reachable by the robot's arm
+            bool objectIsInsideReachableSphere = (distanceFromShoulderToObject < armLength);
+
+
+            if (true) //(objectIsInsideReachableSphere)// (true)
+            {
+                yInfo()<<"Point is inside reachable sphere";
+                double lengthToAdd = 0.1;
+                extendIndexFingerLength(lengthToAdd);
+                pointIndexFingerToTarget(targetObjectPosition);
+                extendIndexFingerLength(-lengthToAdd);
+            }
+            else
+            {
+                yInfo()<<"Point is outside reachable sphere";
+
+                // Pose = 6d
+                yarp::sig::Vector closestReachablePose = getClosestReachablePoseToObject(
+                                                                    targetObjectPosition,
+                                                                    shoulderPosition,
+                                                                    armLength);
+
+                yarp::sig::Vector position(3);
+                yarp::sig::Vector orientation(3);
+                position[0] = closestReachablePose[0];
+                position[1] = closestReachablePose[1];
+                position[2] = closestReachablePose[2];
+
+                orientation[0] = closestReachablePose[3];
+                orientation[1] = closestReachablePose[4];
+                orientation[2] = closestReachablePose[5];
+
+                arm->goToPoseSync(position, orientation);
+
+                bool motion_done;
+                arm->checkMotionDone(&motion_done);
+                while (!motion_done)
+                {
+                    yInfo()<<"Waiting for motion to finish...";
+                    // some verbosity
+                    //printStatus();
+                    Time::delay(1.0);
+                    arm->checkMotionDone(&motion_done);
+                }
+
+                yInfo()<<"done!";
+            }
+            yInfo()<<"Pointing done!";
+
+            tempTotalPointings++;
+
+            if (tempTotalPointings < 5)
+            {
+                threadCalled = false;
+            }
+            else
+            {
+                close();
+            }
+        }
+
         return true;
     }
 
@@ -394,9 +442,6 @@ int main(int argc, char *argv[])
         yError()<<"YARP doesn't seem to be available";
         return 1;
     }
-
-//            yarp::sig::Vector shoulderPosition = findShoulderPosition();
-
 
     CtrlModule mod;
     ResourceFinder rf;
